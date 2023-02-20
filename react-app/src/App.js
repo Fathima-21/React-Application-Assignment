5ab579b2422006c8c30390d87a58d85d0ba856a9
Fathima-21 <94165835+Fathima-21@users.noreply.github.com>
2023-02-20 22:59:32 +0530

    Add files via upload

---
+++ react-app/src/App.js
@@ -0,0 +1,28 @@
+import React,{useState} from "react";
+import './App.css';
+import { Login } from './components/Login';
+import {Register} from './components/Register'
+
+
+
+
+function App() {
+  //for login page to be displayed 1st
+  const [currentForm,setCurrentForm] = useState('login');
+  const toggleForm = (formName) =>{
+    setCurrentForm(formName);
+
+  }
+  return (
+    <div className='App'>
+      {
+        currentForm ==="login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
+      }
+     
+     
+    </div>
+  
+  );
+}
+
+export default App;
